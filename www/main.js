42823423d78ac7ab1accafa6c26e4a6f77266491
Gur Kohli <gkohli@sfu.ca>
2016-09-26 16:29:22 -0700

    Refactoring directory + Fixing CSS issues

--- www/main.js
+++ www/main.js
@@ -1,7 +1,7 @@
 var myResume = angular.module('myResume', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 myResume.controller("rootCtrl", function($scope, $http) {
-	$http.get('data.json')
+	$http.get('www/data.json')
 			.then(function(res) {
 				$scope.model = res.data;
 			})
