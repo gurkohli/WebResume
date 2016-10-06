195bdf056be7f1147cdf136fb45ea2bd3c52f4a8
Gur Kohli <gkohli@sfu.ca>
2016-10-05 21:22:37 -0700

    Updated Website

--- www/main.js
+++ www/main.js
@@ -1,9 +1,12 @@
 var myResume = angular.module('myResume', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-myResume.controller("rootCtrl", function($scope, $http) {
+myResume.controller("rootCtrl", function($scope, $http, $sce) {
 	$http.get('www/data.json')
 			.then(function(res) {
 				$scope.model = res.data;
 			})
 
+	$scope.trustAsHtml = function(string) {
+    	return $sce.trustAsHtml(string);
+	};
 });
